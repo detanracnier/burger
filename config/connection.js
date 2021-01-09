4d7e74d8f8fd20e30686829f8115909ba747bca1
Spencer Rosio <spencerrosio3d@gmail.com>
2021-01-08 19:24:25 -0600

    i cant spell

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 let mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    host: "localhost",
+    host: "j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "czt4m44avebxfahk",
     password: "j2v6vnrcg2wwwaqx",
